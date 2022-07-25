33c111033a633b55dbfdb26e188be2448396e696
sriram143647 <kusumasriram143@gmail.com>
2022-07-25 19:00:50 +0530

    commit32

--- django_project1/project1/settings.py
+++ django_project1/project1/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/kolkata'
 
 USE_I18N = True
 USE_L10N = True
@@ -127,3 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SESSION_COOKIE_AGE = 10800
+# SESSION_COOKIE_NAME = 'session_cookie'
+# SESSION_ENGINE = 'django.contrib.sessions.backends.file'
+# SESSION_FILE_PATH = os.path.join(BASE_DIR,'session')
--- django_project1/project1/views.py
+++ django_project1/project1/views.py
@@ -3,7 +3,34 @@ from django.http import HttpResponse as response
 
 # Create your views here.
 def home(request):
-    return render(request,'project_templates/home.html')
+    request.session['name']='sriram123'
+    request.session.set_expiry(3600)
+    res = render(request,'project_templates/home.html')
+    # res.set_cookie(key='name',value='sriram',max_age=360,domain='http://127.0.0.1:8000/',secure=True,httponly=True,samesite='Strict')
+    # res.set_signed_cookie(key='name',value='sriram',salt='abc@123',max_age=360,domain='http://127.0.0.1:8000/',secure=True,httponly=True,samesite='Strict')
+    return res
+
+def  get_session(request):
+    name = request.session['name']
+    if 'name' in request.session:
+        request.session.modified = True
+        # print(request.session.keys())
+        # print(request.session.items())
+        # print(request.session.get_session_cookie_age())
+        # print(request.session.get_expiry_age())
+        # print(request.session.get_expiry_date())
+        # print(request.session.get_expire_at_browser_close())
+        # fname = request.session.setdefault('fname','kusuma')
+        return render(request,'get_session.html',{'name':name})
+    else:
+        return response("Your Sessin has Expired.....")
+    
+def del_session(request):
+    # if 'name' in request.session:
+    #     del request.session['name']
+    request.session.flush()
+    request.session.clear_expired()
+    return render(request,'del_session.html')
 
 def index(request):
     return render(request,'project_templates/index.html')
