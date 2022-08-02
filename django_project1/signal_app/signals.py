6ffe5139484b1ede69af44041a2c3e142d26ec37
sriram143647 <kusumasriram143@gmail.com>
2022-08-02 17:06:25 +0530

    models_signals

--- django_project1/signal_app/signals.py
+++ django_project1/signal_app/signals.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.signals import user_logged_in,user_logged_out,user_login_failed
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.db.models.signals import *
 
+# login signals
 @receiver(user_logged_in,sender=User)
 def user_login(sender,request,user,**kwargs):
     print('user logged in successfully')
@@ -24,11 +26,80 @@ def user_logout(sender,request,user,**kwargs):
 user_logged_out.connect(user_logout,sender=User)
 
 @receiver(user_login_failed)
-def user_login(sender,credentials,request,**kwargs):
+def user_login_fai(sender,credentials,request,**kwargs):
     print('user login failed')
     print('---------------------------')
     print('Sender:',sender)
     print('Request:',request)
     print('credentials:', credentials)
+    print(f'kwargs {kwargs}')
+    print('---------------------------')
+
+#model signals
+@receiver(pre_save,sender=User)
+def save_beginning(sender,instance,**kwargs):
+    print('pre-save signal waved')
+    print('---------------------------')
+    print('Sender:',sender)
+    print('Instance:',instance)
+    print(f'kwargs {kwargs}')
+    print('---------------------------')
+    
+@receiver(post_save,sender=User)
+def save_ending(sender,instance,created,**kwargs):
+    if created:
+        print('post-save signal waved')
+        print('---------------------------')
+        print('New Record Created')
+        print('Sender:',sender)
+        print('Instance:',instance)
+        print('Created:',created)
+        print(f'kwargs {kwargs}')
+        print('---------------------------')
+    else:
+        print('post-save signal waved')
+        print('---------------------------')
+        print('Record Updated')
+        print('Sender:',sender)
+        print('Instance:',instance)
+        print('Created:',created)
+        print(f'kwargs {kwargs}')
+        print('---------------------------')
+        
+@receiver(pre_delete,sender=User)
+def delete_beginning(sender,instance,**kwargs):
+    print('pre-delete signal waved')
+    print('---------------------------')
+    print('Record About to Deleted')
+    print('Sender:',sender)
+    print('Instance:',instance)
+    print(f'kwargs {kwargs}')
+    print('---------------------------')
+
+@receiver(post_delete,sender=User)
+def delete_ending(sender,instance,**kwargs):
+    print('post-delete signal waved')
+    print('---------------------------')
+    print('Record Deleted')
+    print('Sender:',sender)
+    print('Instance:',instance)
+    print(f'kwargs {kwargs}')
+    print('---------------------------')
+    
+@receiver(pre_init,sender=User)
+def init_beginning(sender,*args,**kwargs):
+    print('pre-init signal waved')
+    print('---------------------------')
+    print('Sender:',sender)
+    print(f'args {args}')
+    print(f'kwargs {kwargs}')
+    print('---------------------------')
+
+@receiver(post_init,sender=User)
+def init_ending(sender,*args,**kwargs):
+    print('post-init signal waved')
+    print('---------------------------')
+    print('Sender:',sender)
+    print(f'args {args}')
     print(f'kwargs {kwargs}')
     print('---------------------------')
