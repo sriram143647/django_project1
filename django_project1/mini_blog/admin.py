9bfcb0ad9af96627ce4a67b89acbedbd76d122f3
sriram <kusumasriram143@gmail.com>
2022-11-24 16:07:21 +0530

    one_to_many_and_many_to_many_relationship

--- django_project1/mini_blog/admin.py
+++ django_project1/mini_blog/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from mini_blog.models import post,like
+from mini_blog.models import post
 
 # Register your models here.
 @admin.register(post)
 class std_admin(admin.ModelAdmin):
-    list_display = ['author','title','desc','publish_date']
+    list_display = ['written_by','title','desc','publish_date']
     
-@admin.register(like)
-class like_admin(admin.ModelAdmin):
-    list_display = ['author','title','desc','publish_date','like']
+# @admin.register(like)
+# class like_admin(admin.ModelAdmin):
+#     list_display = ['author','title','desc','publish_date','like']
--- django_project1/mini_blog/models.py
+++ django_project1/mini_blog/models.py
@@ -9,10 +9,16 @@ class post(models.Model):
     content = models.TextField(max_length=1000)
     publish_date = models.DateField(null=True)
     # author = models.ForeignKey(get_user_model(), related_name="author", on_delete=models.CASCADE) 
-    author = models.OneToOneField(student_data, on_delete=models.CASCADE,primary_key=True)
+    # author = models.OneToOneField(student_data, on_delete=models.CASCADE,primary_key=True)
     # author = models.OneToOneField(student_data, on_delete=models.PROTECT,primary_key=True)
     # author = models.OneToOneField(student_data, on_delete=models.CASCADE,primary_key=True, limit_choices_to={'is_staff':True})
+    # author = models.ForeignKey(student_data, on_delete=models.CASCADE)
+    # author = models.ForeignKey(student_data, on_delete=models.SET_NULL,null=True)
+    author = models.ManyToManyField(student_data)
     
-class like(post):
-    post_like = author = models.OneToOneField(post, on_delete=models.CASCADE,primary_key=True,parent_link=True)
-    likes = models.IntegerField()
+    def written_by(self):
+        return ','.join([str(auth) for auth in self.author.all()])
+    
+# class like(post):
+#     post_like = author = models.OneToOneField(post, on_delete=models.CASCADE,primary_key=True,parent_link=True)
+#     likes = models.IntegerField()
